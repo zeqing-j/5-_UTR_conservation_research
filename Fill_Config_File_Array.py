81216e1ae67aa7b500c8a10057fbb42d9b10969b
Zeqing Jiang <116913781+zeqing-j@users.noreply.github.com>
2025-03-24 00:55:25 -0400

    Update Fill_Config_File_Array.py

--- Fill_Config_File_Array.py
+++ Fill_Config_File_Array.py
@@ -2,22 +2,12 @@ import os
 import sys
 
 def create_config(input_directory, output_filename, num_scripts=38591):
-    """
-    Generate a config file that calls the first num_scripts .sh files using their full paths.
-    """
-    # Define the full path prefix
+    # Generate a config file that calls the first num_scripts .sh files using their full paths.
     path_prefix = "/ocean/projects/bio200049p/zjiang2/Files/5primedata/test_bigbed/bash_dir/"
-
-    # List all .sh files in the input directory
     sh_files = [f for f in os.listdir(input_directory) if f.endswith(".sh")]
-
-    # Sort the list to ensure consistent order
     sh_files.sort()
 
-    # Take only the first num_scripts
     selected_files = sh_files[:num_scripts]
-
-    # Create the config file content
     with open(output_filename, 'w') as f:
         for i,j in enumerate(selected_files):
             f.write(f"bash {path_prefix}{j}\n")
